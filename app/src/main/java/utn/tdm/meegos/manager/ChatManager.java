3048ed7e1ca5d417d11b21f8623ee2a44873fc06
Eze Fer <ezequiel004@gmail.com>
2019-10-07 21:30:09 -0300

    Se agrega findChatsByAlias.

--- app/src/main/java/utn/tdm/meegos/manager/ChatManager.java
+++ app/src/main/java/utn/tdm/meegos/manager/ChatManager.java
@@ -1,28 +1,68 @@
 package utn.tdm.meegos.manager;
 
-import android.os.AsyncTask;
+import android.content.Context;
+import android.database.Cursor;
 
-import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.Comparator;
 
-import utn.tdm.meegos.Task.ServerTask;
-import utn.tdm.meegos.util.XMLDataBlock;
+import utn.tdm.meegos.database.EventsSQLiteHelper;
+import utn.tdm.meegos.domain.Chat;
+import utn.tdm.meegos.preferences.MeegosPreferences;
 
 public class ChatManager {
-    private ServerTask userRegisterTask;
 
-    public void registerUser(String username, String password){
-        XMLDataBlock requestBodyBlock = new XMLDataBlock("action",null, null);
-        XMLDataBlock actionDetailBlock = new XMLDataBlock("action-detail",requestBodyBlock, null);
-        Hashtable<String, String> user = new Hashtable<>();
-        user.put("username", username);
-        user.put("password", password);
-        XMLDataBlock userBlock = new XMLDataBlock(
-                "user",
-                actionDetailBlock,
-                user
-        );
-//        new ServerTask(null).execute(requestBodyBlock);
+    private Context context;
+    EventsSQLiteHelper eventsSQLiteHelper;
 
-        int a = 0;
+    public ChatManager(Context context) {
+        this.context = context;
+        this.eventsSQLiteHelper = new EventsSQLiteHelper(context, EventsSQLiteHelper.DB_NAME, null, EventsSQLiteHelper.CURRENT_DB_VERSION);
+    }
+
+    public void saveChat(long timestamp, String from, String to, String message) {
+        eventsSQLiteHelper.insertChat(timestamp, to, from, message);
+    }
+
+    public ArrayList<Chat> findChatsByAlias(String alias) {
+        ArrayList<Chat> chats = new ArrayList<>();
+        String selection = "";
+        //TODO: Usar PREFERNCES para la busqueda
+//        if (MeegosPreferences.isch HistoryCallFiltered(context)) {
+//            selection = "tipo_evento = " + Evento.LLAMADA;
+//            if (MeegosPreferences.isHistorySMSFiltered(context)) {
+//                selection += " OR tipo_evento = " + Evento.SMS;
+//            }
+//        } else if (MeegosPreferences.isHistorySMSFiltered(context)) {
+//            selection = "tipo_evento = " + Evento.SMS;
+//        }
+        Cursor cursor = eventsSQLiteHelper.findChatsByAlias("to LIKE '" + alias + "' OR from LIKE '" + alias + "'");
+        while (cursor.moveToNext()) {
+            chats.add(
+                new Chat(
+                    cursor.getLong(0),
+                    cursor.getLong(1),
+                    cursor.getString(2),
+                    cursor.getString(3),
+                    cursor.getString(4)
+                )
+            );
+        }
+        cursor.close();
+
+        // Ordenamos el resultado
+        //TODO: Setear el ordenamiento
+        chats.sort(new Comparator<Chat>() {
+            @Override
+            public int compare(Chat c1, Chat c2) {
+                if (MeegosPreferences.getHistoryOrder(context).equals("fecha ASC")) {
+                    return Long.compare(c2.getTimestamp(), c1.getTimestamp());
+                } else {
+                    return Long.compare(c1.getTimestamp(), c2.getTimestamp());
+                }
+            }
+        });
+
+        return chats;
     }
 }
