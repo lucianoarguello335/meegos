63a6b0e31075c9aedc166b3fe161deec2c25eb6d
Eze Fer <ezequiel004@gmail.com>
2019-10-07 21:28:29 -0300

    Se agrega filtro en la lista de Contactos.

--- app/src/main/java/utn/tdm/meegos/preferences/MeegosPreferences.java
+++ app/src/main/java/utn/tdm/meegos/preferences/MeegosPreferences.java
@@ -22,9 +22,8 @@ public class MeegosPreferences {
         return getPrefs(context).getString("preference_contact_criteria", "ASC");
     }
 
-    // TODO: filtro para el listado de contactos
-    public static String getContactFilter(Context context) {
-        return getPrefs(context).getString("contact_filter", "ALL");
+    public static Boolean isContactHasPhoneNumberFiltered(Context context) {
+        return getPrefs(context).getBoolean("preference_contact_has_phone_number", false);
     }
 
     public static String getHistoryOrder(Context context) {
@@ -66,6 +65,26 @@ public class MeegosPreferences {
     public static void setPassword(Context context, String value) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putString("chat_password", value);
+        editor.commit();
+    }
+
+    /**
+     * Value to use in Calendar.getInstance().setTimeInMillis()
+     * @param context
+     * @return
+     */
+    public static Long getTimestamp(Context context) {
+        return getPrefs(context).getLong("chat_timestamp", 0);
+    }
+
+    /**
+     * Value of Calendar.getInstance().getTimeInMillis()
+     * @param context
+     * @param milliseconds time as UTC milliseconds from the epoch.
+     */
+    public static void setTimestamp(Context context, Long milliseconds) {
+        SharedPreferences.Editor editor = getPrefs(context).edit();
+        editor.putLong("chat_timestamp", milliseconds);
         editor.commit();
     }
 }
