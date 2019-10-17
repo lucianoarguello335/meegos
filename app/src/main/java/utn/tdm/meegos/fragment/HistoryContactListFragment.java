79626bf940bf03b0799c7e1a78a78024bd945cff
Eze Fer <ezequiel004@gmail.com>
2019-10-17 10:01:19 -0300

    Se mejora el historyContactList.

--- app/src/main/java/utn/tdm/meegos/fragment/HistoryContactListFragment.java
+++ app/src/main/java/utn/tdm/meegos/fragment/HistoryContactListFragment.java
@@ -22,20 +22,7 @@ import utn.tdm.meegos.domain.Evento;
 import utn.tdm.meegos.listener.OnListEventListener;
 import utn.tdm.meegos.manager.EventoManager;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
- * interface.
- */
 public class HistoryContactListFragment extends Fragment implements OnListEventListener {
-
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
-    private OnListFragmentInteractionListener mListener;
-    private OnListEventListener onListEventListener;
 
     private EventoManager eventoManager;
     private HistoryContactListAdapter historyContactListAdapter;
@@ -48,25 +35,6 @@ public class HistoryContactListFragment extends Fragment implements OnListEventL
     public HistoryContactListFragment() {
     }
 
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static HistoryContactListFragment newInstance(int columnCount) {
-        HistoryContactListFragment fragment = new HistoryContactListFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.history_contact_list_fragment, container, false);
@@ -75,11 +43,7 @@ public class HistoryContactListFragment extends Fragment implements OnListEventL
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
             final RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
             eventoManager = new EventoManager(getContext());
             contactBundle = getActivity().getIntent().getExtras();
 
@@ -89,27 +53,7 @@ public class HistoryContactListFragment extends Fragment implements OnListEventL
                             contactBundle.getString("contact_lookup"),
                             contactBundle.getString("contact_nombre")
                     ),
-                    new OnListEventListener() {
-                        @Override
-                        public void onDeleteEvent(Evento evento) {
-//                            Eliminamos el evento
-                            int result = eventoManager.deleteEvento(evento);
-//                            Notificamos el resultado
-                            if (result > 0) {
-                                Toast.makeText(getContext(), R.string.deleted_success, Toast.LENGTH_LONG).show();
-                            } else {
-                                Snackbar.make(getView(), R.string.deleted_failed, Snackbar.LENGTH_LONG).show();
-                            }
-//                            Actualizamos la coleccion del adapter
-                            historyContactListAdapter.setEventos(eventoManager.findEventosByContact(
-                                    evento.getContactoId(),
-                                    evento.getContactoLookup(),
-                                    evento.getContactoNombre()
-                            ));
-//                            ejecutamos el notifyDataSetChanged
-                            historyContactListAdapter.notifyDataSetChanged();
-                        }
-                    }
+                    this
             );
 
             recyclerView.setAdapter(historyContactListAdapter);
@@ -118,25 +62,23 @@ public class HistoryContactListFragment extends Fragment implements OnListEventL
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnListFragmentInteractionListener) {
-            mListener = (OnListFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnListFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    @Override
     public void onDeleteEvent(Evento evento) {
-
+//        Eliminamos el evento
+        int result = eventoManager.deleteEvento(evento);
+//        Notificamos el resultado
+        if (result > 0) {
+            Toast.makeText(getContext(), R.string.deleted_success, Toast.LENGTH_LONG).show();
+        } else {
+            Snackbar.make(getView(), R.string.deleted_failed, Snackbar.LENGTH_LONG).show();
+        }
+//          Actualizamos la coleccion del adapter
+        historyContactListAdapter.setEventos(eventoManager.findEventosByContact(
+                evento.getContactoId(),
+                evento.getContactoLookup(),
+                evento.getContactoNombre()
+        ));
+//          ejecutamos el notifyDataSetChanged
+        historyContactListAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -150,10 +92,5 @@ public class HistoryContactListFragment extends Fragment implements OnListEventL
             ));
             historyContactListAdapter.notifyDataSetChanged();
         }
-    }
-
-    public interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onListFragmentInteraction(Evento evento);
     }
 }
